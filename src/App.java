e95bb0485f1f00414ab4bb3296e1f89e3a787277
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-10-15 17:39:13 +0100

    feat: clase app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import net.salesianos.utiles.process.ProcessLauncher;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String texto = " ";
+        BufferedReader reader = new BufferedReader(new FileReader("./src/data/text.txt", StandardCharsets.UTF_8));
+        String linea = reader.readLine();
+        while (linea != null) {
+            texto += linea;
+            linea = reader.readLine();
+        }
+        reader.close();
+
+        Process subProcessComa = ProcessLauncher.initContadorProcess(texto, ",", "totalComa.txt");
+        Process subProcessPunto = ProcessLauncher.initContadorProcess(texto, ".", "totalPunto.txt");
+        Process subProcessDosPuntos = ProcessLauncher.initContadorProcess(texto, ":", "totalDosPuntos.txt");
+        Process subProcessPuntoYComa = ProcessLauncher.initContadorProcess(texto, ";", "totalPuntoyComa.txt");
+        subProcessComa.waitFor();
+        subProcessPunto.waitFor();
+        subProcessDosPuntos.waitFor();
+        subProcessPuntoYComa.waitFor();
+
     }
 }
